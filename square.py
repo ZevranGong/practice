3ad36206ea1ca05b9edd8fbd72cdc0ace29267de
Zevran <ziyuang@andrw.cmu.edu>
2015-10-17 17:22:02 -0400

    practice 1

---
+++ square.py
@@ -0,0 +1,36 @@
+#Calculate the minimum numbers required whose square sum is equal to a given
+#number using DP and memoization in Python
+
+def memoize(f):
+    memo={}
+    end = 101
+    for i in xrange(end):
+        memo[i**2] = 1
+    def helper(x):
+        if x not in memo:
+            memo[x]=f(x)
+        return memo[x]
+    return helper
+    end = 101
+    for i in xrange(end):
+        d[i**2] = 1
+    return d
+
+@memoize
+def answer(n):
+    temp=[]
+    if n==2:
+        return 2
+    elif n==3:
+        return 3
+    else:
+        largestNumber = int(n**.5)
+        for i in range(largestNumber-1,0,-1):
+            if (n-i**2 == 0):
+                return 1
+            temp += [answer(n-i**2)]
+        return 1+min(temp)
+        
+            
+            
+        
